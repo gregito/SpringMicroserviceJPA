aeb6d8c0a0fc5a891a8f98d6700415aa36b84b8c
Meszaros Gergely <grigorijat9026@gmail.com>
2018-05-27 21:24:15 +0200

    Initial commit

---
+++ src/main/java/com/example/microservices/todomicroservices/ToDoMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.todomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoMicroservicesApplication.class, args);
+	}
+}
