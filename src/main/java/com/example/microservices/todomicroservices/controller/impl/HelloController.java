b0b411cc1a2dc87dbcccc6cd64e7e8221a5ca7b4
Mészáros Gergely <grigorijat9026@gmail.com>
2018-06-02 23:29:19 +0200

    removed /userInOutput Hello endpoint option

--- src/main/java/com/example/microservices/todomicroservices/controller/impl/HelloController.java
+++ src/main/java/com/example/microservices/todomicroservices/controller/impl/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.microservices.todomicroservices.controller.impl;
 
 import com.example.microservices.todomicroservices.controller.interfaces.HelloEndpoint;
-import com.example.microservices.todomicroservices.entities.User;
 import com.example.microservices.todomicroservices.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +13,6 @@ public class HelloController implements HelloEndpoint {
 
     public String sayHello() {
         return helloService.sayHello();
-    }
-
-    @Override
-    public User giveMeAUser() {
-        return helloService.getRandomUser();
     }
 
 }
--- src/main/java/com/example/microservices/todomicroservices/controller/interfaces/HelloEndpoint.java
+++ src/main/java/com/example/microservices/todomicroservices/controller/interfaces/HelloEndpoint.java
@@ -1,14 +1,10 @@
 package com.example.microservices.todomicroservices.controller.interfaces;
 
-import com.example.microservices.todomicroservices.entities.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface HelloEndpoint {
 
     @RequestMapping("/hello")
     String sayHello();
-
-    @RequestMapping("/userInOutput")
-    User giveMeAUser();
 
 }
